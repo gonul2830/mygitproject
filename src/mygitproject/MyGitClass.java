bb9e21bf3979e183c3eddd30dacccb21a504d14e
gonul2830 <pirisauruspdf@gmail.com>
2021-05-23 20:31:02 +0300

    version 3

--- src/mygitproject/MyGitClass.java
+++ src/mygitproject/MyGitClass.java
@@ -5,6 +5,7 @@ public class MyGitClass {
 	public static void main(String[] args) {
 		System.out.println("version 1");
 		System.out.println("My second commit");
+		System.out.println("my third commit");
 		
 
 	}
